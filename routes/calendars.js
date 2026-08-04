e642ef7805940bbb202fbd9c604c58460f6c20ca
Jeremy Madrid <395340+slicethendice@users.noreply.github.com>
2026-08-03 22:11:24 -0500

    Expose household calendar routes

--- routes/calendars.js
+++ routes/calendars.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const router = express.Router();
-const calendarsController = require("../controllers/calendarsController"); // Correct path
+const calendarsController = require("../controllers/calendarsController");
 
-// Route to get all calendars for the authenticated user
+const router = express.Router();
+
 router.get("/", calendarsController.getCalendars);
-router.get("/:userId", calendarsController.getCalendarsByUserId); // NEW ROUTE
 router.post("/selections", calendarsController.updateCalendarSelections);
 router.get("/:calendarId/events", calendarsController.getEvents);
 
